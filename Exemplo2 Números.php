823ef29bd409b8c03fc713609529bfc4bda44d28
Zhunixy <kauan8643@gmail.com>
2022-04-01 09:24:42 -0300

    Treino

---
+++ Exemplo2 Números.php
@@ -0,0 +1,37 @@
+<?php
+
+$n1 = 2;
+$n2 = 4;
+$n3 = 6;
+//Resultado igual a 2
+$rp1 = $n2 - $n1;
+//Resultado igual a 8
+$rp2 = $n3 + $n1;
+//Resultado igual a 12
+$rp3 = $n1 * $n3;
+//Resultado igual a 2
+$rp4 = $n2 / $n1;
+//Resultado igual a 2, já que o resto da divisão de 8 por 6 é igual a 2
+$rp5 = $n2 % $n3;
+
+
+echo $rp1;
+echo '<br>';
+echo $rp2;
+echo '<br>';
+echo $rp3;
+echo '<br>';
+echo $rp4;
+echo '<br>';
+
+// (-) = Subtração
+//(+) = Adição
+//(*) = Multiplicação
+//(/) = Divisão
+//(%) = Divisão
+
+// RP= Respostas
+//N1= Números de 1 a 3
+
+
+?>
---
+++ Exemplo3 Números com++.php
@@ -0,0 +1,29 @@
+<?php
+
+$num1 = 2;
+$num2 = 4;
+$num3 = 6;
+$num4 = 8;
+
+//Resultado igual a 3
+$resposta1 = ++$num2 - $num1;
+//Resultado igual a 8
+$resposta2 = $num3-- + $num1;
+//Resultado igual a 9
+$resposta3= --$num1+ $num4;
+
+echo $resposta1;
+echo '<br>';
+echo $resposta2;
+echo '<br>'
+echo $resposta3;
+
+
+
+// -operando: troca o sinal do operando.
+// ++operando: conhecido como pré-incremento, primeiro incrementa depois realiza a operação.
+// --operando: conhecido como pré-decremento, primeiro decrementa depois realiza a operação.
+// operando++: conhecido como pós-incremento, primeiro faz a operação depois incrementa.
+// operando--: conhecido como pós-decremento, primeiro faz a operação depois decrementa.
+
+?>
