d20f05adf126e26087a0e8cff89856a7b0ebefd7
Zhunixy <kauan8643@gmail.com>
2022-04-11 10:08:18 -0300

    Php

---
+++ Fixando PHP (PESSOAL)/ex1.php
@@ -0,0 +1,30 @@
+<?php
+
+$nm = "Meu nome é:";
+$nm2 = "Tenho:";
+$nm3 = "Moro em:";
+$nm4 = "Faço o curso de:";
+
+$nome = " Kauan";
+$idade = 15;
+$cidade= " Lins";
+$curso= " DS";
+$idade2 = ' Anos';
+
+$rp1 = $nm . $nome;
+$rp2 = $nm2 . $idade . $idade2;
+$rp3 = $nm3 . $cidade;
+$rp4 = $nm4 . $curso;
+
+echo $rp1;
+echo '<br>';
+echo $rp2;
+echo '<br>';
+echo $rp3;
+echo '<br>';
+echo $rp4;
+
+?>
+
+
+
---
+++ Fixando PHP (PESSOAL)/ex2.php
@@ -0,0 +1,19 @@
+<?php
+
+$n1 = 20;
+$n2 = 40;
+$n3 = 60;
+$n4 = 1;
+$n5 = 5;
+
+$res1 = $n1 + $n2;
+$res2 = $n5 * $n3;
+$res3 = $n4 - $n3;
+
+echo $res1;
+echo '<br>';
+echo $res2;
+echo '<br>';
+echo $res3;
+
+?>
